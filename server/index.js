b027df4bd38369a1c9671c5815efabbcfa8d0767
jonasalmeida <jalmeida@mathbiol.org>
2018-07-24 22:43:20 +0000

    mongodb url

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 exports.handler = (event, context, callback) => {
-    var fs=require('fs')
+    var fs=require('fs');
+    var conn = require('./connection.json');
+    var mongo = require('mongodb').MongoClient
+    var url = 'mongodb://'+conn.username+':'+conn.password+'@'+conn.url
     // var path=require('path')
     // var aws=require('aws-sdk')
     // var conn=JSON.parse(fs.readFileSync('connection.json','utf8'))
     // TODO implement
-    callback(null,'upload att '+Date());
+    mongo.connect(url, function(err, db) {
+        var cursor = db.collection('praia').find();
+        cursor.each(function(err, doc) {
+            console.log(doc);
+        });
+    }); 
+    callback(null,'upload at '+Date()+' for url '+url);
 };
