a86e43ced86e7896499ba09b1aa3daa8ee70ed48
jonasalmeida <jalmeida@mathbiol.org>
2018-07-24 04:47:28 +0000

    mongodb dependency

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
 exports.handler = (event, context, callback) => {
+    var fs=require('fs')
+    // var path=require('path')
+    // var aws=require('aws-sdk')
+    // var conn=JSON.parse(fs.readFileSync('connection.json','utf8'))
     // TODO implement
-    callback(null,'upload at '+Date());
+    callback(null,'upload att '+Date());
 };
